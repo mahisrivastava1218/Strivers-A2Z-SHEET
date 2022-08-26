28d019fdb807b078d41fa9fceb892737b439b64e
mahisrivastava1218 <98544175+mahisrivastava1218@users.noreply.github.com>
2022-08-26 14:08:37 +0530

    Create Sum of first n terms .java

---
+++ Sum of first n terms .java
@@ -0,0 +1,10 @@
+class Solution {
+    long sumOfSeries(long N) {
+        // code here
+        long sum=0;
+        sum= (((N+1)*N)/2)*(((N+1)*N)/2);
+            
+        
+        return sum;
+    }
+}
